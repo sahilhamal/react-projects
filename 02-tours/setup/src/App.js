7fbd08bd8f53db428facaacc5031f62fef484a0b
sahilhamal <sahil_hamal@yahoo.com>
2022-04-17 16:17:13 -0400

    project2

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((x) => x.id !== id);
+    setTours(newTours);
+  };
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const resp = await fetch(url);
+      const tours = await resp.json();
+      setIsLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <main>
+          <Loading></Loading>
+        </main>
+      </>
+    );
+  }
+
+  if (tours.length == 0) {
+    return (
+      <main>
+        <h2>No tours left</h2>
+        <button onClick={() => fetchTours()} className="btn">
+          {" "}
+          Refresh
+        </button>
+      </main>
+    );
+  }
+  return (
+    <>
+      <main>
+        <Tours tours={tours} removeTour={removeTour}></Tours>
+      </main>
+    </>
+  );
 }
 
-export default App
+function App2() {
+  return <h2>Tours Project Setup</h2>;
+}
+
+export default App;
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [showLess, setShowLess] = useState(true);
+  return (
+    <div className="single-tour">
+      <img src={image} alt={name}></img>
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+      </footer>
+      <p>{showLess ? `${info.substring(0, 200)}...` : info}</p>
+      <button onClick={() => setShowLess(!showLess)}>
+        {showLess ? "More" : "Less"}
+      </button>
+      <button className="delete-btn" onClick={() => removeTour(id)}>
+        Not Interested
+      </button>
+    </div>
+  );
 };
 
 export default Tour;
